bbc64aaf38a1d7713f8a88a476a0d15b6b726df9
Nate <palmern22@gmail.com>
2012-10-17 13:10:05 -0400

    rooms file for defining rooms

---
+++ rooms.py
@@ -0,0 +1,12 @@
+# This file will contain room definitions
+# unaccaptble room names are things like room1() room2() etc.
+# use your best judgement on what to call a room.
+
+import pygame
+import walls
+
+from walls import *
+
+def playerHouse():
+	wall_list = pygame.sprite.RenderPlain()
+	# List of walls here
